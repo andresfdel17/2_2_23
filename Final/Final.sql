e472204dc4def0b2e3948216bcb122dfcd4c945a
“camilogiral” <ccgiraldo053@misena.edu.com>
2023-03-23 21:16:12 -0500

    BD ejericcio final

--- Final/Final.sql
+++ Final/Final.sql
@@ -23,4 +23,49 @@ create table factura (
 	foreign key (id_empleado) references empleado(id)
 );
 --Crear procediemiento guardar en empleado
-create 
+create procedure usp_guardar_empleado
+@name varchar(50),
+@last_name varchar(50),
+@email varchar(30),
+@phone varchar(30)
+as 
+begin
+	insert into empleado (name,last_name,email,phone)
+	values (@name,@last_name,@email,@phone);
+end
+--- crear procedimiento borrar
+
+create procedure usp_delete_empleado
+@id int
+as 
+begin 
+	delete from empleado
+	where id = @id;
+end
+---- creaer consultar 
+create procedure usp_consultar_empleado
+@id int
+as 
+begin
+	select * from empleado
+	where id = @id
+end 
+-- crear actualizar 
+create procedure usp_update_empleado
+@id int,
+@name varchar(50),
+@last_name varchar(50),
+@email varchar(30),
+@phone varchar(30)
+as 
+begin
+	 update empleado set name=@name, last_name=@last_name, email=@email, phone=@phone
+	 where id = @id
+end
+-- crear listar
+create procedure usp_listar_empleado
+as 
+begin
+	select * from empleado
+end
+	 
