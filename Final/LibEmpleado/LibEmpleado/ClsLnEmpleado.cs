838738b6508e3ce63e71a972f13a984e532c4ee7
andresfdel17 <andresfdel13@hotmail.com>
2023-03-24 20:24:07 -0500

    LN de empleado

--- Final/LibEmpleado/LibEmpleado/ClsLnEmpleado.cs
+++ Final/LibEmpleado/LibEmpleado/ClsLnEmpleado.cs
@@ -4,15 +4,19 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+//Librerias externas de Adalberto
+using LibConexionBD;
+using LibLlenarGrids;
 
 namespace LibEmpleado
 {
     public class ClsLnEmpleado
     {
         #region ATRIBUTOS
-        //Id del empleado
+        // Id del empleado
         private int id = 0;
-        //Nombre
+        // Nombre
         private string name = String.Empty;
         // Apelidos
         private string last_name = String.Empty;
@@ -38,8 +42,119 @@ namespace LibEmpleado
         public SqlDataReader EmReader { get => emReader; set => emReader = value; }
         #endregion
         #region METODOS PUBLICOS
-        public ClsLnEmpleado() {}
+        public ClsLnEmpleado() { }
+        public bool GuardarEmpleado()
+        {
+            try
+            {
+                ClsConexion objC = new ClsConexion();
+                string query = "EXECUTE usp_save_empleado '" + name + "','" + last_name + "','" + email + "','" + phone + "'";
+                if (!objC.EjecutarSentencia(query, false))
+                {
+                    this.error = objC.Error;
+                    objC = null;
+                    return false;
+                }
+                this.resultado = "Empleado guardado correctamente";
+                objC = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+                return false;
+            }
+        }
+        public bool EditarEmpleado()
+        {
+            try
+            {
+                ClsConexion objC = new ClsConexion();
+                string query = "EXECUTE usp_update_empleado '" + name + "','" + last_name + "','" + email + "','" + phone + "'";
+                if (!objC.EjecutarSentencia(query, false))
+                {
+                    this.error = objC.Error;
+                    objC = null;
+                    return false;
+                }
+                this.resultado = "Empleado editado correctamente";
+                objC = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+                return false;
+            }
+        }
+        public bool EliminarEmpleado()
+        {
+            try
+            {
+                ClsConexion objC = new ClsConexion();
+                string query = "EXECUTE usp_delete_empleado " + id;
+                if (!objC.EjecutarSentencia(query, false))
+                {
+                    this.error = objC.Error;
+                    objC = null;
+                    return false;
+                }
+                this.resultado = "Empleado eliminado correctamente";
+                objC = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+                return false;
+            }
+        }
+        public bool ConsultarEmpleado()
+        {
+            try
+            {
+                ClsConexion objC = new ClsConexion();
+                string query = "EXECUTE usp_consultar_empleado " + id;
+                if (!objC.Consultar(query, false))
+                {
+                    this.error = objC.Error;
+                    objC = null;
+                    return false;
+                }
+                emReader = objC.Reader;
+                objC = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+                return false;
+            }
+        }
+        public bool ListarEmpleado(DataGridView grid)
+        {
+            try
+            {
+                ClsLLenarGrids objL = new ClsLLenarGrids
+                {
+                    NombreTabla = "empleado",
+                    SQL = "usp_listar_empleado"
+                };
+                if (!objL.LlenarGrid(grid))
+                {
+                    this.error = objL.ERROR;
+                    objL = null;
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+                return false;
+            }
 
+        }
         #endregion
     }
 }
